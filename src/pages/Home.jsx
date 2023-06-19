1ba72ff169622a61b1ea59e2dea9320d39318bf5
Noman Waseem <nomanwaseem111@gmail.com>
2023-06-20 03:49:23 +0500

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import FeaturesSection from '../components/FeaturesSection'
 import Frequently from '../components/Frequently'
 import HeroSection from '../components/HeroSection'
-import Portfolio from '../components/portfolio'
+import Portfolio from '../components/Portfolio';
 import Services from '../components/Services'
 import About from '../components/About'
 import Blog from '../components/Blog'
